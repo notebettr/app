471ad97b482ed27eb5c24890b93d35ba94fdc8e2
Anna Johnson <anna@desertform.com>
2023-05-09 15:28:31 -0700

    Style log in page

--- lib/app_web/live/user_login_live.ex
+++ lib/app_web/live/user_login_live.ex
@@ -3,33 +3,38 @@ defmodule AppWeb.UserLoginLive do
 
   def render(assigns) do
     ~H"""
-    <div class="mx-auto max-w-sm">
-      <.header class="text-center">
-        Welcome back!
+    <div class="mx-auto max-w-sm font-jost">
+      <.header class=" flex justify-center">
+        <img src="/images/notebettrlogo.svg" />
       </.header>
+      <div class="flex flex-col items-center justify-center bg-white shadow-lg rounded-md pb-6">
+        <.simple_form for={@form} id="login_form" action={~p"/users/log_in"} phx-update="ignore">
+          <p class="text-2xl text-center">Welcome back!</p>
+          <.input field={@form[:email]} type="email" label="Email" required />
+          <.input field={@form[:password]} type="password" label="Password" required />
 
-      <.simple_form for={@form} id="login_form" action={~p"/users/log_in"} phx-update="ignore">
-        <.input field={@form[:email]} type="email" label="Email" required />
-        <.input field={@form[:password]} type="password" label="Password" required />
-
-        <:actions>
-          <.input field={@form[:remember_me]} type="checkbox" label="Keep me logged in" />
-          <.link href={~p"/users/reset_password"} class="text-sm font-semibold">
-            Forgot your password?
-          </.link>
-        </:actions>
-        <:actions>
-          <.button phx-disable-with="Signing in..." class="w-full">
-            Sign in <span aria-hidden="true">→</span>
-          </.button>
-        </:actions>
-      </.simple_form>
-        <span>
-          Don't have an account yet?
-          <.link navigate={~p"/users/register"} class="font-semibold text-brand hover:underline">
-            Create one.
-          </.link>
-        </span>
+          <:actions>
+            <.input field={@form[:remember_me]} type="checkbox" label="Keep me logged in" />
+            <.link href={~p"/users/reset_password"} class="text-sm font-semibold">
+              Forgot your password?
+            </.link>
+          </:actions>
+          <:actions>
+            <.button
+              phx-disable-with="Signing in..."
+              class="mx-auto w-32 bg-pink text-white py-2 px-10"
+            >
+              Let's go!
+            </.button>
+          </:actions>
+        </.simple_form>
+      </div>
+      <div class="mt-4 text-center">
+        Don't have an account yet?
+        <.link navigate={~p"/users/register"} class="font-semibold text-blue hover:underline">
+          Get started here.
+        </.link>
+      </div>
     </div>
     """
   end
