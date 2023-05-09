57a932f31920697a1e847c409206c72700e7d274
Anna Johnson <anna@desertform.com>
2023-05-09 10:27:57 -0700

    Add Auth

--- config/dev.exs
+++ config/dev.exs
@@ -3,9 +3,9 @@ import Config
 # Configure your database
 config :app, App.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "m0msSp@ghetti",
   hostname: "localhost",
-  database: "app_dev",
+  database: "postgres",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
